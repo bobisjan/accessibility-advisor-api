5f87d4b982a4932117c0c44c876f2a0264644b4f
Jan Bobisud <bobisjan@fel.cvut.cz>
2012-11-22 12:42:12 +0100

    Improve fixtures creation

--- data/model/fixtures/answers.php
+++ data/model/fixtures/answers.php
@@ -1,6 +1,44 @@
 <?php
 
-$answers = array();
+
+
+$questions = array();
+
+$questions[] = array(
+    'id' => 1,
+    'disabled' => array(2, 3, 4, 5, 6),
+    'sql' => "SELECT Impairment.idImpairment AS id, Impairment.title AS title FROM Impairment ORDER BY Impairment.idImpairment;"
+);
+
+$questions[] = array(
+    'id' => 2,
+    'disabled' => array(),
+    'sql' => "SELECT Disability.idDisability AS id, Disability.ICFCodeQualifiedTitle AS title FROM Disability WHERE Impairment_idImpairment = 1;"
+);
+
+$questions[] = array(
+    'id' => 3,
+    'disabled' => array(),
+    'sql' => "SELECT Age_Group.idAge_Group as id, Age_Group.groupName AS title FROM Age_Group;"
+);
+
+$questions[] = array(
+    'id' => 4,
+    'disabled' => array(),
+    'sql' => "SELECT Context_Of_Use.idContext_Of_Use AS id, Context_Of_Use.title AS title FROM Context_Of_Use WHERE contextCathegory='lighting';"
+);
+
+$questions[] = array(
+    'id' => 5,
+    'disabled' => array(),
+    'sql' => "SELECT Context_Of_Use.idContext_Of_Use AS id, Context_Of_Use.title AS title FROM Context_Of_Use WHERE contextCathegory='noisiness'"
+);
+
+$questions[] = array(
+    'id' => 6,
+    'disabled' => array(),
+    'sql' => "SELECT Application_Type.idApplication_Type AS id, Application_Type.title AS title FROM Application_Type"
+);
 
 
 
@@ -9,149 +47,29 @@ function answers($sql) {
 	$answers = array();
 	
 	foreach ($database->fetchAll($sql) as $value) {
-	    $answers[] = $value->title;
+	    $answers[] = Nette\ArrayHash::from($value);
 	}
 	return $answers;
 }
 
 
 
-/* ******************** Answer ******************** */
-
+$answers = array();
 $id = 1;
-$i = 1;
-$question = 1;
-$disabled = array(2, 3, 4, 5, 6);
-$sql = "SELECT Impairment.title AS title FROM Impairment ORDER BY Impairment.idImpairment;";
 
-foreach (answers($sql) as $item) {
-	$answer = new Nette\ArrayHash();
-	$answer->id = $id;
-	$answer->title = $item;
-	$answer->disabled = in_array($id, $disabled);
-	$answer->question_id = $question;
-	
-	$answer->backend = $i;
-	$i++;
-	
-	$answers[$id] = $answer;
-	$id++;
+foreach ($questions as $value) {
+    foreach (answers($value['sql']) as $item) {
+    	$answer = new Nette\ArrayHash();
+    	$answer->id = $id;
+    	$answer->title = $item->title;
+    	$answer->disabled = in_array($id, $value['disabled']);
+    	$answer->question_id = $value['id'];
+    	
+    	$answer->backend = $item->id;
+    	
+    	$answers[$id] = $answer;
+    	$id++;
+    }
 }
-
-
-
-
-/* ******************** Answer ******************** */
-
-$i = 108;
-$question = 2;
-$disabled = array();
-$sql = "SELECT Disability.ICFCodeQualifiedTitle AS title FROM Disability WHERE Impairment_idImpairment = 1;";
-
-foreach (answers($sql) as $item) {
-	$answer = new Nette\ArrayHash();
-	$answer->id = $id;
-	$answer->title = $item;
-	$answer->disabled = in_array($id, $disabled);
-	$answer->question_id = $question;
-	
-	$answer->backend = $i;
-	$i++;
-	
-	$answers[$id] = $answer;
-	$id++;
-}
-
-
-
-/* ******************** Answer ******************** */
-
-$i = 1;
-$question = 3;
-$disabled = array();
-$sql = "SELECT Age_Group.groupName AS title FROM Age_Group;";
-
-foreach (answers($sql) as $item) {
-	$answer = new Nette\ArrayHash();
-	$answer->id = $id;
-	$answer->title = $item;
-	$answer->disabled = in_array($id, $disabled);
-	$answer->question_id = $question;
-	
-	$answer->backend = $i;
-	$i++;
-	
-	$answers[$id] = $answer;
-	$id++;
-}
-
-
-
-/* ******************** Answer ******************** */
-
-$i = 1;
-$question = 4;
-$disabled = array();
-$sql = "SELECT Context_Of_Use.title AS title FROM Context_Of_Use WHERE contextCathegory='lighting';";
-
-foreach (answers($sql) as $item) {
-	$answer = new Nette\ArrayHash();
-	$answer->id = $id;
-	$answer->title = $item;
-	$answer->disabled = in_array($id, $disabled);
-	$answer->question_id = $question;
-	
-	$answer->backend = $i;
-	$i++;
-	
-	$answers[$id] = $answer;
-	$id++;
-}
-
-
-
-/* ******************** Answer ******************** */
-
-$question = 5;
-$disabled = array();
-$sql = "SELECT Context_Of_Use.title AS title FROM Context_Of_Use WHERE contextCathegory='noisiness'";
-
-foreach (answers($sql) as $item) {
-	$answer = new Nette\ArrayHash();
-	$answer->id = $id;
-	$answer->title = $item;
-	$answer->disabled = in_array($id, $disabled);
-	$answer->question_id = $question;
-	
-	$answer->backend = $i;
-	$i++;
-	
-	$answers[$id] = $answer;
-	$id++;
-}
-
-
-
-/* ******************** Answer ******************** */
-
-$i = 1;
-$question = 6;
-$disabled = array();
-
-foreach (array("Desktop application", "Web application", "Mobile application") as $item) {
-	$answer = new Nette\ArrayHash();
-	$answer->id = $id;
-	$answer->title = $item;
-	$answer->disabled = in_array($id, $disabled);
-	$answer->question_id = $question;
-	
-	$answer->backend = $i;
-	$i++;
-	
-	$answers[$id] = $answer;
-	$id++;
-}
-
-
 
 return $answers;
